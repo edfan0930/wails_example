b114fb32698bca1260c593310032d41e90871ca0
Ed <red@redde-Mac-mini.local>
2020-05-25 18:40:33 +0800

    add video base64

---
+++ frontend/src/components/VideoUpload.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <Upload
+        multiple
+        type="drag"
+        :before-upload ="beforeUpload"
+        action="//jsonplaceholder.typicode.com/posts/"
+    >
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>Click or drag files here to upload</p>
+        </div>
+    </Upload>
+    <div>
+    <video  width="400" controls>
+  <source src="videoSrc" type="video/mp4">
+  Your browser does not support HTML video.
+</video>
+</div>
+</div>
+</template>
+<script>
+    export default {
+        name: 'VideoUpload',
+        methods: {
+            beforeUpload:function(f){
+                console.log('19',f);
+                console.log('20',f.size)
+               const reader = new FileReader()
+               reader.onload = f => {
+                   this.videoSrc=f.target.result
+                   console.log('video src',this.videoSrc)
+               }
+               //reader.onload = e => this.$emit('load', e.target.result)
+                reader.readAsDataURL(f)
+                console.log('25',reader)
+                return false
+            }
+        },
+        data() {
+            return {
+                videoSrc:'',
+            }
+        },
+        computed: {
+            
+        },
+    }
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,12 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import App from './App.vue';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
+Vue.use(ViewUI);
 
 import * as Wails from '@wailsapp/runtime';
 
